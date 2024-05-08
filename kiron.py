5ecc782780c965377cbc7a84e044720760ec6ccf
AYYUB-XD <162326392+AYYUB-XD@users.noreply.github.com>
2024-05-08 12:26:53 +0600

    Update kiron.py

--- kiron.py
+++ kiron.py
@@ -1,9 +1,5 @@
-import os, sys, platform
-try:os.system('git pull')
-except:pass
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import kiron
-    kiron.approval()
-elif bit == '32bit':
-    os.system("exit")
+import os, sys
+try:
+    __import__("kiron").approval()
+except Exception as e:
+    exit(str(e))
