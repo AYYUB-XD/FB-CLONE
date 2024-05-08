57f9e5a45f0f20ed7c076bd225e5e23ef2c0a1d5
AYYUB-XD <162326392+AYYUB-XD@users.noreply.github.com>
2024-05-08 12:51:00 +0600

    Update kiron.py

--- kiron.py
+++ kiron.py
@@ -1,5 +1,6 @@
-import os, sys
-try:
-    __import__("kiron").approval()
-except Exception as e:
-    exit(str(e))
+import os, platform
+bit = platform.architecture()[0]
+if "64bit" in bit:
+  import kiron
+else:
+  print(" Not Supported your 32 bit device ")
